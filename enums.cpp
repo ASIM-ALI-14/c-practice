dea74112726e2857f8fb8a4d78dcd08373d0f495
Asim Ali <dev.asimali14@gmail.com>
2025-03-21 11:19:38 +0500

    new

---
+++ enums.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(){
+  system("cls");
+  enum level{
+    low=12,
+    medium=23,
+    high=34
+  };
+   enum level l1 =medium;
+    cout<<l1;
+}
